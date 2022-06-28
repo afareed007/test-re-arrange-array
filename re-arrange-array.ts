27c712ef27f1631240771027936c0095903f093b
aTechChap <afareed92@gmail.com>
2022-06-28 08:44:11 +0100

    feat: add function to re-arrange an array
    add function to re-arrange an array with inline comments

---
+++ re-arrange-array.ts
@@ -0,0 +1,32 @@
+"use strict";
+/**
+ * re-arrange a given array into
+ * an array of 'N' number of chuncks 
+ */
+
+/**
+ * 
+ * @param arr input array for 
+ * @param n number of chunks 
+ * @returns 2-D array of chunks
+ */
+const groupArrayElements = function (arr: number[], n: number) {
+  const res: number[][] = [];
+  for (let i = 0; i < arr.length; i += n) {
+    const part = arr.slice(i, i + n);
+    res.push(part);
+  }
+  return res;
+};
+
+function main() {
+  console.log("main started");
+  
+  let result = groupArrayElements([1, 2, 3, 4, 5], 2);
+  console.log("result: ");
+  console.log(result);
+
+  return;
+}
+
+main();
